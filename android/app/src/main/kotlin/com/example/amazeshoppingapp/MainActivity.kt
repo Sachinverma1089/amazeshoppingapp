c9ca78193305cfb0d699fc044abe99a3e95be2be
Sachin Verma <vermasachin2392@gmail.com>
2024-07-15 05:06:46 -0700

    server

---
+++ android/app/src/main/kotlin/com/example/amazeshoppingapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.amazeshoppingapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ server/index.js
@@ -0,0 +1,48 @@
+// imports from packages
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+
+
+//imports from other files
+
+const authRouter=require('./routes/auth.js');
+const adminRouter = require('./routes/admin.js');
+const productRouter = require('./routes/product.js');
+
+
+
+//init
+const PORT = 3000;
+const DB = process.env.DB
+const app = express();
+
+
+//middleware   // CLIENT -> middleware SERVER -> CLIENT
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+
+
+//Connections
+mongoose
+.connect(DB)
+.then(()=>{
+    console.log("Connection Sunccessful");
+})
+.catch((e) => {
+    console.log(e);
+})
+
+
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log('Server running on http://0.0.0.0:3000');
+})
+// localhost
