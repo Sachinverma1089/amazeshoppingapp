a1b2fb2fcae225915afa7e8959f1f676047e8b2b
Sachin Verma <vermasachin2392@gmail.com>
2024-07-19 01:30:02 -0700

    nothing

--- server/models/order.js
+++ server/models/order.js
@@ -1,41 +1,37 @@
-const mongoose = require('mongoose');
-const { productSchema } = require('./product');
-
+const mongoose = require("mongoose");
+const { productSchema } = require("./product");
 
 const orderSchema = mongoose.Schema({
-    products:[
-        {
-            product:productSchema,
-            quantity:{
-                type:Number,
-                required:true,
-            }
-        }
-    ],
-    totalPrice:{
-        type:Number,
-        required:true,
-    },
-    address:{
-        type:String,
-        required:true,
-    },
-    userId: {
+  products: [
+    {
+      product: productSchema,
+      quantity: {
+        type: Number,
         required: true,
-        type: mongoose.Schema.Types.ObjectId, // Change to ObjectId
-        ref: 'User' // Reference to the User model
+      },
     },
-    orderedAt:{
-        type:Date,
-        default:Date.now,
-        
-    },
-    status:{
-        type:Number,
-        default:0,
-    }
-
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    required: true,
+    type: String,
+  },
+  orderedAt: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Number,
+    default: 0,
+  },
 });
 
-const Order = mongoose.model('Order',orderSchema);
-module.exports=Order;
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
--- server/routes/user.js
+++ server/routes/user.js
@@ -84,23 +84,27 @@ userRouter.post('/api/save-user-address',auth,async(req,res)=>{
 });
 
 // order product
-userRouter.post('/api/order',auth,async(req,res)=>{
-  try {
-    const {cart,totalPrice,address}=req.body;
-    let products=[];
 
-    for(let i=0;i<cart.length;i++){
+userRouter.post("/api/order", auth, async (req, res) => {
+  try {
+    const { cart, totalPrice, address } = req.body;
+    let products = [];
+
+    for (let i = 0; i < cart.length; i++) {
       let product = await Product.findById(cart[i].product._id);
-      if(product.quantity>=cart[i].quantity){
-        product.quantity-=cart[i].quantity;
-        products.push({product,quantity:cart[i].quantity});
+      if (product.quantity >= cart[i].quantity) {
+        product.quantity -= cart[i].quantity;
+        products.push({ product, quantity: cart[i].quantity });
         await product.save();
-      }else{
-        return res.status(400).json({msg:`${product.name} is out of stock!`});
+      } else {
+        return res
+          .status(400)
+          .json({ msg: `${product.name} is out of stock!` });
       }
     }
+
     let user = await User.findById(req.user);
-    user.cart=[];
+    user.cart = [];
     user = await user.save();
 
     let order = new Order({
@@ -108,28 +112,24 @@ userRouter.post('/api/order',auth,async(req,res)=>{
       totalPrice,
       address,
       userId: req.user,
-      orderedAt: Date.now(),
+      orderedAt: new Date().getTime(),
     });
     order = await order.save();
     res.json(order);
-    
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({error:error.message});
-    
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
 
 //user order
 
-userRouter.get('/api/orders/me',auth,async(req,res)=>{
+userRouter.get("/api/orders/me", auth, async (req, res) => {
   try {
-    let orders = await Order.find({userId:req.user});
+    const orders = await Order.find({ userId: req.user });
     res.json(orders);
-  } catch (error) {
-    res.status(500).json({error:error.message});
-    
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
-})
+});
 module.exports=userRouter;
