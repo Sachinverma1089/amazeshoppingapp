7a28b2ec6f8fc826aa4d327188b0a6355658945b
Sachin Verma <vermasachin2392@gmail.com>
2024-07-18 20:50:02 -0700

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:amazon_clone_project/constants/global_variables.dart';
-import 'package:amazon_clone_project/features/admin/screens/admin_screen.dart';
-import 'package:amazon_clone_project/features/auth/services/auth_services.dart';
-import 'package:amazon_clone_project/providers/user_provider.dart';
-import 'package:amazon_clone_project/router.dart';
+
+import 'package:amazeshoppingapp/constants/global_variables.dart';
+import 'package:amazeshoppingapp/features/admin/screens/admin_screen.dart';
+import 'package:amazeshoppingapp/features/auth/services/auth_services.dart';
+import 'package:amazeshoppingapp/providers/user_provider.dart';
+import 'package:amazeshoppingapp/router.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'common/widgets/bottom_bar.dart';
 import 'features/auth/screens/auth_screen.dart';
 import 'features/home/screens/home_screen.dart';
