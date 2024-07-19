e83c06c41ab3afa3fb3a7d3a0737b8482e040eed
Sachin Verma <vermasachin2392@gmail.com>
2024-07-19 01:40:56 -0700

    minor changes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -111,18 +111,19 @@ adminRouter.get('/admin/analytics',admin,async(req,res)=>{
     }
 });
 
-async function fetchCategoryWiseProduct(category){
-    let earnings=0;
-    let categoryOrders= await Order.find({
-        'products.product.category':category,
+async function fetchCategoryWiseProduct(category) {
+    let earnings = 0;
+    let categoryOrders = await Order.find({
+      "products.product.category": category,
     });
-
-    for(let i=0;i<categoryOrders.length;i++){
-        for(let j=0;j<orders[i].products.length;j++){
-            earnings+= orders[i].products[j].quantity* orders[i].products[j].product.price
-        }
+  
+    for (let i = 0; i < categoryOrders.length; i++) {
+      for (let j = 0; j < categoryOrders[i].products.length; j++) {
+        earnings +=
+          categoryOrders[i].products[j].quantity *
+          categoryOrders[i].products[j].product.price;
+      }
     }
     return earnings;
-
-}
+  }
 module.exports=adminRouter;
